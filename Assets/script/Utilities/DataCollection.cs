27f8f14f31be4c5b2d94d18533c604569db91a75
Xwwd 667 <2605226954@qq.com>
2025-02-14 20:10:57 +0800

    2/14更新

---
+++ Assets/script/Utilities/DataCollection.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+[System.Serializable]
+public class ItemDetails
+{
+    public int itemID;
+
+    public string name;
+
+    public ItemType itemType;
+
+    public Sprite itemIcon;
+
+    public Sprite itemOnWorldSprite;
+
+    public int itemUseRadius;
+
+    public bool itemcanPickedup;
+
+    public bool itemcanCarried;
+
+    public bool itemcanDropped;
+
+    public int itemPrice;
+
+    public float sellPercentage;
+}
+ 
---
+++ Assets/script/Utilities/Enums.cs
@@ -0,0 +1,10 @@
+public enum ItemType
+{
+
+    Seed, Commodity, Furniture,
+    HoeTool, ChopTool, BreakTool, ReapTool, WaterTool, CollectTool,
+    ReapableScenery
+    //种子，商品,家具，
+    //锄头，斧头 ，凿石头，割草，浇水，收割，
+    //可以被割的杂草
+}
